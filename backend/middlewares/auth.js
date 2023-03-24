c757d1d1b69969ce86c438e5e5e551e0b25d0cc0
Jury Orava <jury.orava@yandex.ru>
2023-03-24 10:27:30 +0300

    keys

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,9 +10,9 @@ const extractBearerToken = (header) => {
   return header.replace('Bearer ', '');
 };
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  const { NODE_ENV, JWT_SECRET } = process.env;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return handleAuthError(res, next);
@@ -22,7 +22,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return handleAuthError(res, next);
   }
